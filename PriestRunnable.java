c81c7e61b2718e72894f1e0aebd61c9fdf3f89a5
cogiorgio <33833721+cogiorgio@users.noreply.github.com>
2019-04-13 17:23:40 +0200

    Add files via upload

---
+++ PriestRunnable.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class PriestRunnable implements Runnable {
+    private Priest pr;
+    public PriestRunnable(Priest pr){
+      this.pr=pr;
+    }
+    public void run() {
+        try {
+            ServerSocket s = new ServerSocket(pr.port);
+            while(pr.listening==1) {
+                Socket client = s.accept();
+            }
+        }catch(Exception e){
+            System.out.println("errore nella generazione del socket");
+        };
+    }
+}
